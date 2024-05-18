23e4f31f3ab3a1b4d78f564256f6e5d65f0b6430
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-05-18 15:00:40 +0500

    navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import { connectToDb } from "@/database/connection"
-import { getUser } from "@/lib/user"
-import Link from "next/link"
-import { Button } from "./ui/button"
-import { logout } from "@/actions/user"
+import { connectToDb } from "@/database/connection";
+import { getUser } from "@/lib/user";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { logout } from "@/actions/user";
 
 const Navbar = async () => {
-  await connectToDb()
-  const user = await getUser()
+  await connectToDb();
+  const user = await getUser();
 
   return (
     <nav class="bg-white border-gray-200 dark:bg-gray-900 mt-5">
@@ -70,7 +70,6 @@ const Navbar = async () => {
               </li>
             )}
 
-<<<<<<< HEAD
             {!user && (
               <>
                 {" "}
@@ -102,52 +101,7 @@ const Navbar = async () => {
         </div>
       </div>
     </nav>
-  )
-=======
-        <nav class="bg-white border-gray-200 dark:bg-gray-900 mt-5">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                
-                <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="/logo.svg" class="h-14" alt="Study Logo" />
-                </a>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        
+  );
+};
 
-                        <li>
-                            <Link href="/" class="block py-2 px-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500" aria-current="page">Home</Link>
-                        </li>
-
-                        <li>
-                            <Link href="/dashboard/public" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Dashboard</Link>
-                        </li>
-
-                        <li>
-                            <Link href="/profile" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Profile</Link>
-                        </li>
-                        
-                        <li>
-                            <Link href="/signup" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Signup</Link>
-                        </li>
-                        <li>
-                            <Link href="/login" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</Link>
-                        </li>
-
-                       
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        
-
-    )
->>>>>>> ba0d39841c537e3a8664c24472134c84988b08ce
-}
-
-export default Navbar
+export default Navbar;
