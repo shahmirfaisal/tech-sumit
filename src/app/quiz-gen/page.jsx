5874ad9de002552af0e15d6cf2c4a5e4eb67d0ab
muquitarif <muquitarif2@gmail.com>
2024-05-18 15:11:02 +0500

    a

--- src/app/quiz-gen/page.jsx
+++ src/app/quiz-gen/page.jsx
@@ -2,8 +2,7 @@ const page = () => {
     return (
 
         <div>
-            <div data-paperform-id="2mqasbvm"></div><script>var script = document.createElement('script'); script.src = "https://paperform.co/__embed.min.js"; 
-            document.body.appendChild(script); </script>
+         
         </div>
     )
 }
