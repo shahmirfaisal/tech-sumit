e2d7057bbca3be7f95f18a23e942a46d586c5034
muquitarif <muquitarif2@gmail.com>
2024-05-18 14:30:13 +0500

    icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
 
         <nav class="bg-white border-gray-200 dark:bg-gray-900 mt-5">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                
                 <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="/logo.svg" class="h-14" alt="Study Logo" />
                 </a>
@@ -43,6 +44,7 @@ const Navbar = () => {
                 </div>
             </div>
         </nav>
+        
 
     )
 }
