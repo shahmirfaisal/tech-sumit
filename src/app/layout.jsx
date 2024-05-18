c5c3dc9c84f4e983c5aebc71ab7216ec2db134d0
muquitarif <muquitarif2@gmail.com>
2024-05-18 13:48:30 +0500

    added logo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,8 @@
+import Navbar from "@/components/Navbar"
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { Toaster } from "sonner"
+import Footer from "@/components/Footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar></Navbar>
         {children}
+        <Footer></Footer>
 
         <Toaster position="top-center" />
       </body>
