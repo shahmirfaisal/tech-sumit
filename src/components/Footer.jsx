5b064ff869d6b686879667e198bfcb79f848d828
muquitarif <muquitarif2@gmail.com>
2024-05-18 14:04:58 +0500

    s

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,45 @@
+import Link from "next/link"
+
 const Footer = () => {
  
   return (
         
 
 <footer class="bg-white dark:bg-gray-900">
+    
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="#" class="flex items-center">
-                  <img src="/logo.svg" class="h-14 me-3" alt="FlowBite Logo" />
+                  <img src="/logo.svg" class="h-14 me-3" alt="Study.ai Logo" />
               </a>
           </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-4">
               <div>
                   <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Study.ai</a>
+                          <Link href="/" class="hover:underline">Study.ai</Link>
                       </li>
-                      <li>
-                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                      <li class="mb-4">
+                          <Link href="/dashboard/public" class="hover:underline">Dashboard</Link>
+                      </li>
+                  </ul>
+              </div>
+
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Account</h2>
+                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                      <li class="mb-4">
+                          <Link href="/profile" class="hover:underline">Profile</Link>
+                      </li>
+                      <li class="mb-4">
+                          <Link href="/login" class="hover:underline">Login</Link>
+                      </li>
+
+                      <li class="mb-4">
+                          <Link href="/signup" class="hover:underline">Signup</Link>
                       </li>
                   </ul>
               </div>
@@ -27,10 +47,10 @@ const Footer = () => {
                   <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
+                          <a href="#" class="hover:underline ">Github</a>
                       </li>
                       <li>
-                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+                          <a href="#" class="hover:underline">Discord</a>
                       </li>
                   </ul>
               </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,9 +17,20 @@ const Navbar = () => {
                 </button>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        
+
                         <li>
                             <Link href="/" class="block py-2 px-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500" aria-current="page">Home</Link>
                         </li>
+
+                        <li>
+                            <Link href="/dashboard/public" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Dashboard</Link>
+                        </li>
+
+                        <li>
+                            <Link href="/profile" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Profile</Link>
+                        </li>
+                        
                         <li>
                             <Link href="/signup" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Signup</Link>
                         </li>
@@ -27,9 +38,6 @@ const Navbar = () => {
                             <Link href="/login" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</Link>
                         </li>
 
-                        <li>
-                            <Link href="/dashboard/public" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Dashboard</Link>
-                        </li>
                        
                     </ul>
                 </div>
