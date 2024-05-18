377da58d2c7d8144603369071d3837bd15f988d9
muquitarif <muquitarif2@gmail.com>
2024-05-18 15:26:07 +0500

    d

--- src/app/quiz-gen/page.jsx
+++ src/app/quiz-gen/page.jsx
@@ -4,10 +4,8 @@ const page = () => {
         <div>
 
 
-            <form class="max-w-sm mx-auto">
+            <form class="max-w-sm mx-auto text-center">
                 <h1 className="font-bold text-center text-3xl pb-10">Test Generator</h1>
-
-
                 <button type="submit" class="text-white text-center bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Generate</button>
             </form>
 
